2e78ebd95509edcb8950161358f222127cbfa83e
Krzysztof <swietojanski@gmail.com>
2017-08-07 19:17:08 +0200

    zmiana nazwy bazy danych
    zmienilem nazwe bazy danych na overtime

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-  $dbhost = 'overtime';     //adres serwera sql
+  $dbhost = 'overtime';     //adres serwera MySQL np. 127.0.0.1
   $dblogin = 'overtime';    //login uzytkownika bazy danych
   $dbpass = 'overtime';     //haslo
-  $dbselect = 'sqlover';   //nazwa bazy danych
+  $dbselect = 'overtime';   //nazwa bazy danych
   mysql_connect($dbhost,$dblogin,$dbpass);
   mysql_select_db($dbselect) or die("Błąd przy wyborze bazy danych");
   mysql_query("SET CHARACTER SET UTF8");
